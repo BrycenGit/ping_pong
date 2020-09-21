12c4e5a7f05e3b3b439146dee36c42a2274cd4d8
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-21 09:44:04 -0700

    fail test, returns pong for multiples of 5
    Co-Authored-By: Drew Parker <drewjparker91@gmail.com>
    co-authored-by: Donny Mays <dtmays2@gmail.com>

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -9,4 +9,8 @@ describe('#ping_pong') do
   it('returns "ping" for multiples of 3') do
     expect(ping_pong(4)).to(eq([1,2,"ping",4]))
   end
+
+  it('returns "pong" for multiples of 5') do
+    expect(ping_pong(5)).to(eq([1,2,"ping",4,"pong"]))
+  end
 end
